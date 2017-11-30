40cecc4b919f2c9988f4418be47b11e01f735134
kadimawl <kadimawl@163.com>
2017-11-30 14:48:16 +0800

    image

---
+++ src/views/outHead.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="headeBox">
+    <div class="imgBox"></div>
+    二级子路由--登陆注册等头部
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+      return {}
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+*{
+  margin: 0;
+  padding: 0;
+}
+.headeBox{
+  height: 97px;
+  width: 1200px;
+  margin: 0 auto;
+  padding: 0 20px;
+}
+</style>
