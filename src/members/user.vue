bc85af88f5658924b434b598b17d4061d443ac20
yishuihan <1326983019qq.com>
2017-11-30 17:04:42 +0800

    'jj'

---
+++ src/members/user.vue
@@ -0,0 +1,41 @@
+<template>
+<div class="memeber">
+    <!-- <select/> -->
+    <div class="member-body">
+         <user/>
+         <router-view/>
+    </div>
+</div>
+  
+</template>
+
+<script>
+// import select from 
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+* {
+  margin: 0;
+  padding: 0;
+}
+.dispf{
+  display: flex;
+}
+.member{
+  .member-body{
+      .dispf;
+}
+}
+
+
+
+  
+  
+ 
+</style>
---
+++ src/members/userevalTop.vue
@@ -0,0 +1,43 @@
+<template>
+<div class="memeber">
+    <!-- <select/> -->
+    <div class="member-body">
+         <user/>
+         <router-view/>
+    </div>
+</div>
+  
+</template>
+
+<script>
+// import select from 
+import user from './user'
+export default {
+  data() {
+    return {};
+  },
+   components: { user }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+* {
+  margin: 0;
+  padding: 0;
+}
+.dispf{
+  display: flex;
+}
+.member{
+  .member-body{
+      .dispf;
+}
+}
+
+
+
+  
+  
+ 
+</style>
