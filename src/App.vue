f8db87762440e3875d8b22cf721ca66e727bf3b9
kadimawl <kadimawl@163.com>
2017-12-03 14:01:59 +0800

    wl

--- src/App.vue
+++ src/App.vue
@@ -13,11 +13,7 @@ import sele from "./components/sele";
 import bottom from "./components/bottom";
 export default {
   name: "app",
-<<<<<<< HEAD
-  components: { top, bottom } //需要在当前组件内注册（添加）
-=======
   components: { top, sele, bottom } //需要在当前组件内注册（添加）
->>>>>>> 7170c881c05e937e94070163087260405826f2d0
 };
 </script>
 
