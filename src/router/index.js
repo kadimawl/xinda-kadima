4bbd1546166ca5fd3b01272fcc56f68e307d8b25
kadimawl <kadimawl@163.com>
2017-12-04 17:01:08 +0800

    wl package

--- src/router/index.js
+++ src/router/index.js
@@ -17,19 +17,6 @@ import accountsetBody from '@/members/accountsetBody'
 import changePd from '@/members/changePd'
 
 
-<<<<<<< HEAD
-import Order from '@/components/Order' //订单详情http://localhost:9090
-import detial from '@/components/detial' //商品详情
-import shopIndex from '@/components/shopIndex' //店铺首页
-import shopList from '@/components/shopList' //店铺列表
-
-
-import order from '@/components/order/order' //dingdan
-import paymentSuccess from '@/components/order/paymentSuccess' //支付成功
-import paymentFailure from '@/components/order/paymentFailure' //支付失败
-import payZfb from '@/components/order/payZfb' //支付宝支付
-import payBank from '@/components/order/payBank' //银行支付
-=======
 
 import detial from '@/components/detial'//商品详情
 import shopIndex from '@/components/shopIndex'//店铺首页
@@ -42,32 +29,11 @@ import paymentFailure from '@/components/order/paymentFailure'//支付失败
 import payZfb from '@/components/order/payZfb'//支付宝支付
 import payBank from '@/components/order/payBank'//银行支付
 
->>>>>>> b94f9efa3f8b06890802c03d6b1dc9a21117cdbe
 
 
 Vue.use(Router)
 export default new Router({
     routes: [{
-<<<<<<< HEAD
-            path: '/',
-            name: 'HelloWorld',
-            component: HelloWorld
-        }, {
-            path: '/outter',
-            name: 'outter',
-            component: outter,
-            children: [{
-                path: '/login',
-                component: login,
-            }, {
-                path: '/register',
-                component: register
-            }, {
-                path: '/forgetpw',
-                component: forgetpw
-            }]
-        },
-=======
         path: '/',
         name: 'HelloWorld',
         component: HelloWorld
@@ -86,7 +52,6 @@ export default new Router({
             component: forgetpw
         }]
     },
->>>>>>> b94f9efa3f8b06890802c03d6b1dc9a21117cdbe
 
 
 
