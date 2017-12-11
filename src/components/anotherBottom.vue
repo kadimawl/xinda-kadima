b498d97d7da36976c85e81992c417ae79cbef53a
kadimawl <kadimawl@163.com>
2017-12-11 10:41:00 +0800

    wl

---
+++ src/components/anotherBottom.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+      <p>©Copyright 2016 北京信达科技有限公司 京ICP备 16011621号</p>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="less" scoped>
+div{
+    width: 100%;
+    height: 222px;
+    background: #f5f5f5;
+    padding-top: 167px;
+    box-sizing: border-box;
+    p{
+        width: 100%;
+        height: 55px;
+        font-size: 14px;
+        color: #000;
+        line-height: 55px;
+        text-align: center;
+    }
+}
+</style>
