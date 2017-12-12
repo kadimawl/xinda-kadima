587b5943a942f1d45a80bd5f2357ab877513513e
kadimawl <kadimawl@163.com>
2017-12-12 16:12:14 +0800

    wl

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,6 @@ export default {
 </script>
 
 <style lang="less">
-<<<<<<< HEAD
 * {
   margin: 0;
   padding: 0;
@@ -34,13 +33,7 @@ export default {
 a{
   text-decoration: none;
 }
-=======
-  *{
-    margin: 0;
-    padding: 0;
-  }
-  button{
+button{
     outline:0;
   }
->>>>>>> 69b99f888140cdff93e7ba8a109c4d79372323a9
 </style>
