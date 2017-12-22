7f88d083225d9996b38b7a60db65e4b9ab568028
yishuihan <1326983019qq.com>
2017-12-22 09:37:57 +0800

    02

---
+++ src/mobile/stores/store.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="routerBox">
+        
+    </div>
+</template>
+
+<script>
+import mobileBottom from '../mobileBottom'
+export default {
+    components: {mobileBottom}
+}
+</script>
+
+<style lang="less" scoped>
+*{
+    margin: 0;
+    padding: 0;
+}
+.routerBox{
+    min-width: 320px;
+    max-width: 768px;
+    margin: 0 auto;
+    min-height: 568px;
+    max-height: 1280px;
+    width: 100%;
+    height: 100%;
+}
+</style>
---
+++ src/mobile/stores/wxshopList.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="routerBox">
+        <!-- 这是wx端一级路由，所有wx端都挂这下面 -->
+        <router-view/>
+        <mobileBottom/>
+    </div>
+</template>
+
+<script>
+import mobileBottom from '../mobileBottom'
+export default {
+    components: {mobileBottom}
+}
+</script>
+
+<style lang="less" scoped>
+*{
+    margin: 0;
+    padding: 0;
+}
+.routerBox{
+    min-width: 320px;
+    max-width: 768px;
+    margin: 0 auto;
+    min-height: 568px;
+    max-height: 1280px;
+    width: 100%;
+    height: 100%;
+}
+</style>
